56d8610fe739a86bacc3cc09f29e3840bead7210
Amgad <amgad.mourad18@gmail.com>
2023-09-26 06:05:36 +0300

    allowed host

--- football_site/settings.py
+++ football_site/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wtbu)gpb!=wqjj!ulafh@g7)km0_7a@*u(*!b-z6(==_u+5ckf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.51.170.47']
 
 
 # Application definition
