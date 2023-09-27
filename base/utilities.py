3d6328645900926dbc465aff8deda016dc91971f
Amgad <amgad.mourad18@gmail.com>
2023-09-27 04:03:41 +0300

    firefox drivers final edits

--- base/utilities.py
+++ base/utilities.py
@@ -11,21 +11,20 @@ from datetime import datetime
 from .models import team_data_modes , PlayerInfo
 import os
 from django.conf import settings
-from selenium.webdriver.chrome.options import Options
 
 ################################################# teams functions########################################
 
 def get_team_link(team_name):
-    # Specify the path to the GeckoDriver executable
-    geckodriver_path = os.path.join(settings.STATICFILES_DIRS[0], 'geckodriver')  # Replace with the actual path to geckodriver
 
-    # Create a Firefox service with the executable path
-    firefox_service = FirefoxService(geckodriver_path)
     firefox_options = FirefoxOptions()
     firefox_options.headless = True
-       
+    firefox_options.add_argument(argument="--no-sandbox")
+    firefox_options.add_argument(argument="--headless")
+    firefox_options.add_argument(argument="--disable-gpu")
+    firefox_options.add_argument(argument="--window-size=1920,1080")
+   
     # Initialize the Firefox WebDriver with the service
-    driver = webdriver.Firefox(service=firefox_service, options=firefox_options)
+    driver = webdriver.Firefox(options=firefox_options)
 
    
     try:
@@ -61,11 +60,11 @@ def get_team_data_scrape(team_name,teams_df):
     
     if teams_df[teams_df['name'].str.contains(team_name)].any(axis=None):
 
-        print("helloooo")
+        
         worldfootball_url =  teams_df.loc[teams_df['name'].str.contains(team_name), 'links'].iloc[0]
 
     else:
-        print('nenenee')
+        
         worldfootball_url = get_team_link(team_name)
 
 
@@ -83,18 +82,15 @@ def get_team_data_scrape(team_name,teams_df):
     # firefox_options.headless = True
 
 
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')  # Run Chrome in headless mode
-    chrome_options.add_argument("window-size=1400,1500")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("start-maximized")
-    chrome_options.add_argument("enable-automation")
-    chrome_options.add_argument("--disable-infobars")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_experimental_option( "prefs",{'profile.managed_default_content_settings.javascript': 2})
+    firefox_options = FirefoxOptions()
+    firefox_options.headless = True
+    firefox_options.add_argument(argument="--no-sandbox")
+    firefox_options.add_argument(argument="--headless")
+    firefox_options.add_argument(argument="--disable-gpu")
+    firefox_options.add_argument(argument="--window-size=1920,1080")
+    firefox_options.set_preference("javascript.enabled", False)  # Disable JavaScript
 
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Firefox(options=firefox_options)
 
     try:
 
